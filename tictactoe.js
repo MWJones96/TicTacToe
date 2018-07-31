bdc0668250b0036c25a896ed6eed22f4774a0a73
Matthew Jones <eighthmatthewjones@gmail.com>
2018-07-31 23:21:15 +0100

    Few improvements and improved doc

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,12 @@
-let player = "X";
-let ai = "O";
-let BOARD_SIZE = 3;
+//The player's board icon
+let player = "O";
+//The opponent's board icon
+let ai = "X";
 
+//The list of available squares
 let available_squares = Array.apply(null, {length: 9}).map(Function.call, Number);
 
+//Initialise the board
 init();
 
 /**
@@ -14,6 +17,7 @@ function init()
 	available_squares = Array.apply(null, {length: 9}).map(Function.call, Number);
 	for(i=0;i<available_squares.length;i++)
 	{
+		//Add event listeners for every square
 		document.getElementById(i.toString()).innerHTML = "";
 		document.getElementById(i.toString()).addEventListener("click", process_button);
 	}
@@ -21,11 +25,15 @@ function init()
 
 function process_button()
 {
-	
+	//If the square has not already been selected
 	if(available_squares.indexOf(Number(event.target.id)) > -1)
 	{
-		event.target.innerHTML = "X";
+		//Set icon to player's symbol
+		event.target.innerHTML = player;
+		//Delete square from list available
 		delete available_squares[Number(event.target.id)];
+		
+		//If the player has won, then wrap up the game and remove event listeners
 		if(isWon(player, Number(event.target.id)))
 		{
 			console.log("Player Won!");
@@ -33,6 +41,7 @@ function process_button()
 			return;
 		}
 		
+		//If there are no moves left to make, then the game is a draw
 		if(available_squares.filter(function(el){
 			return el != null;
 		}).length == 0)
@@ -42,6 +51,7 @@ function process_button()
 			return;
 		}
 		
+		//Select first available index for opponent
 		let firstIndex = available_squares.filter(function(el){
 			return el != null;
 		})[0];
@@ -49,7 +59,8 @@ function process_button()
 		console.log(firstIndex);
 		
 		delete available_squares[firstIndex];
-		document.getElementById(firstIndex.toString()).innerHTML = "O";
+		document.getElementById(firstIndex.toString()).innerHTML = ai;
+		//If the AI has won
 		if(isWon(ai, firstIndex))
 		{
 			console.log("AI Won!");
@@ -64,6 +75,9 @@ function process_button()
 	console.log(available_squares);
 }
 
+/**
+	Wraps-up the game by removing event listeners and accepting no more input
+**/
 function wrap_up(text)
 {
 	document.getElementById("finished_text").innerHTML = text;
@@ -83,6 +97,9 @@ function isWon(player, placePos)
 	return (check_column(player, placePos) || check_row(player, placePos) || check_diag(player, placePos));
 }
 
+/**
+	Checks for column win condition
+**/
 function check_column(player, placePos)
 {
 	let col0 = document.getElementById(placePos.toString()).innerHTML;
@@ -92,6 +109,9 @@ function check_column(player, placePos)
 	return (col0 === player && col1 === player && col2 === player);
 }
 
+/**
+	Checks for row win condition
+**/
 function check_row(player, placePos)
 {
 	let offset = Math.floor(placePos / 3);
@@ -100,13 +120,16 @@ function check_row(player, placePos)
 	let r2 = 3*offset + ((placePos + 1) % 3);
 	let r3 = 3*offset + ((placePos + 2) % 3);
 	
-	let col0 = document.getElementById(r1.toString()).innerHTML;
-	let col1 = document.getElementById(r2.toString()).innerHTML;
-	let col2 = document.getElementById(r3.toString()).innerHTML;
+	let row0 = document.getElementById(r1.toString()).innerHTML;
+	let row1 = document.getElementById(r2.toString()).innerHTML;
+	let row2 = document.getElementById(r3.toString()).innerHTML;
 		
-	return (col0 === player && col1 === player && col2 === player);
+	return (row0 === player && row1 === player && row2 === player);
 }
 
+/**
+	Checks diagonals by simple brute-force
+**/
 function check_diag(player, placePos)
 {
 	let diag0 = document.getElementById("0").innerHTML;
